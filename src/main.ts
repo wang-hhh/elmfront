f206ee06fe544d0351346f6e5ce3d6facec859f1
SuJiehao <megasu@qq.com>
2021-09-27 20:47:02 +0800

    08-首页轮播图实现

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,14 @@ import '@/assets/styles/iconfont.less'
 
 import '@/mock/index'
 
-import { Tab, Tabs } from 'vant'
+import { Tab, Tabs, Swipe, SwipeItem } from 'vant'
 
 const app = createApp(App)
 // 注册Vant组件
 app.use(Tab)
 app.use(Tabs)
+app.use(Swipe)
+app.use(SwipeItem)
 
 app.use(router)
 app.mount('#app')
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,10 +3,13 @@
   <AppHeader />
   <!-- 频道组件 -->
   <HomeChannel />
+  <!-- 轮播组件 -->
+  <HomeSwipe />
 </template>
 
 <script setup lang="ts">
 // script setup 只需要导入组件，无需注册
 import AppHeader from '@/components/app-header.vue'
 import HomeChannel from './components/home-channel.vue'
+import HomeSwipe from './components/home-swipe.vue'
 </script>
