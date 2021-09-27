07c6666cc12534e45d58236e1a81c6c2e2cacfba
SuJiehao <megasu@qq.com>
2021-09-27 20:17:02 +0800

    项目mock接口

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import router from './router'
 import '@/assets/styles/base.less'
 import '@/assets/styles/iconfont.less'
 
+import '@/mock/index'
+
 import { Tab, Tabs } from 'vant'
 
 const app = createApp(App)
