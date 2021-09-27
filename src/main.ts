1213234d0cecfa3e9652ca7cca4f406b60a2f9b5
SuJiehao <megasu@qq.com>
2021-09-27 20:03:48 +0800

    05-Vant3组件库使用

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,12 @@ import router from './router'
 import '@/assets/styles/base.less'
 import '@/assets/styles/iconfont.less'
 
-createApp(App).use(router).mount('#app')
+import { Tab, Tabs } from 'vant'
+
+const app = createApp(App)
+// 注册Vant组件
+app.use(Tab)
+app.use(Tabs)
+
+app.use(router)
+app.mount('#app')
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,8 +1,12 @@
 <template>
+  <!-- 头部组件 -->
   <AppHeader />
+  <!-- 频道组件 -->
+  <HomeChannel />
 </template>
 
 <script setup lang="ts">
 // script setup 只需要导入组件，无需注册
 import AppHeader from '@/components/app-header.vue'
+import HomeChannel from './components/home-channel.vue'
 </script>
